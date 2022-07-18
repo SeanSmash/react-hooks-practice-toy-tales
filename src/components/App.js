5ee844d34f7cfc82d9cac3654f61011fa1bae673
Sean Sarokas <sps4462@gmail.com>
2022-07-18 08:10:30 -0400

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,21 +6,69 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toyList, setToyList] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then(r => r.json())
+      .then(data => setToyList(data))
+  }, [toyList])
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function handleNewToy(newToyData){
+    fetch("http://localhost:3001/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "name": newToyData.name,
+        "image": newToyData.image,
+        "likes": newToyData.likes
+      })
+    })
+      .then(r => r.json())
+      .then(data => setToyList([...toyList, data]))
+  }
+
+  function handleDelete(id){
+    const updatedToyList = toyList.filter(toy => {
+      return (toy.id === id ? null : true)
+    })
+    setToyList(updatedToyList)
+  }
+
+  function handleLike(toyData){
+    const updatedToyList = toyList.filter(toy => {
+      return (toy.likes === toyData.likes ? toyData : true)
+    })
+    setToyList(updatedToyList)
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onNewToy={handleNewToy}/> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer 
+        toyList={toyList} 
+        onDelete={handleDelete} 
+        onLike={handleLike} />
     </>
   );
 }
 
 export default App;
+
+/*
+App
+  |_Header
+  |_ ToyForm
+  |_ToyContainer
+    |_ToyCard
+*/
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyForm() {
+function ToyForm({ onNewToy }) {
+  const [newToyName, setNewToyName] = useState("")
+  const [newToyImage, setNewToyImage] = useState("")
+
+
+  function handleNewName(e){
+    setNewToyName(e.target.value)
+  }
+
+  function handleNewImage(e){
+    setNewToyImage(e.target.value)
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    const newToyData = {
+      "name": newToyName, 
+      "image": newToyImage, 
+      "likes": 0}
+    onNewToy(newToyData)
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          onChange={handleNewName}
         />
         <br />
         <input
@@ -17,6 +39,7 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          onChange={handleNewImage}
         />
         <br />
         <input
